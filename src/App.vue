c4a539fd2d763bc9fa5851a7eddcedf2bd00c030
RamosEduardo <ramoscleitoneduardo@gmail.com>
2020-09-14 21:59:01 -0300

    Config heroku

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <div id="app">
-    <!-- <Navbar /> -->
     <Sidebar />
     <router-view />
-    <!-- <Home /> -->
-    <!-- <Sobre /> -->
-    <!-- <Contato /> -->
-    <!-- <Cardapio /> -->
-    <!-- <Eventos /> -->
-    <!-- <fotos-casa /> -->
     <Footer />
   </div>
 </template>
@@ -17,12 +10,6 @@
 
   import Sidebar from "./components/Menu/Sidebar.vue";
   import Footer from '@/components/Footer'
-  // import FotosCasa from '@/Pages/FotosCasa'
-  // import Home from '@/Pages/Home'
-  // import Sobre from '@/Pages/Sobre'
-  // import Contato from '@/Pages/Contato'
-  // import Cardapio from '@/Pages/Cardapio'
-  // import Eventos from '@/Pages/Eventos'
 
   export default {
     name: 'App',
@@ -30,12 +17,6 @@
       Sidebar,
       // Home,
       Footer,
-      // Sobre,
-      // Navbar,
-      // Contato
-      // Cardapio,
-      // Eventos,
-      // FotosCasa
     }
   }
 </script>
@@ -44,14 +25,6 @@
 @import './css/main.css';
 @import './css/util.css';
 @import './fonts/themify/themify-icons.css';
-/* @import '../vendor/bootstrap/css/bootstrap.min.css';
-@import '../fonts/font-awesome-4.7.0/css/font-awesome.min.css';
-/* @import '../vendor/animate/animate.css';
-@import "../vendor/css-hamburgers/hamburgers.min.css";
-@import "../vendor/select2/select2.min.css";
-@import "../vendor/daterangepicker/daterangepicker.css";
-@import "../vendor/slick/slick.css";
-@import "../vendor/lightbox2/css/lightbox.min.css"; */ */
 
 #app {
   font-family: 'Roboto', Helvetica, Arial, sans-serif;
