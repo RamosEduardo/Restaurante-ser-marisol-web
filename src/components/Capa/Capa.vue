855d661643e4ddd4c8cc3ac75391c4accb08f6a4
RamosEduardo <ramoscleitoneduardo@gmail.com>
2020-09-16 12:05:51 -0300

    margin top navbar

--- src/components/Capa/Capa.vue
+++ src/components/Capa/Capa.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section-video parallax100" :style="`background-image: url('${backgroundImage}')`">
-    <div class="content-video t-center p-t-75 p-b-250">
+    <div class="content-video t-center p-t-25 p-b-250">
       <Navbar />
       <h3 class="tit4 t-center p-l-15 p-r-15 p-t-200">
         {{ title }}
